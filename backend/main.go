1627124a27d88b752b5b67707f2e21a73b2a0f6c
niteshchand <niteshchand478@gmail.com>
2026-01-31 12:28:02 +0530

    Update CORS in Backend

--- backend/main.go
+++ backend/main.go
@@ -16,7 +16,7 @@ func main() {
 	r := gin.Default()
 // ✅ CORS config
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000","https://mini-eventify.vercel.app",},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
